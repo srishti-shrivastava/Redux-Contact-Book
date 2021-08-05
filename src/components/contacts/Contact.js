3ebeba62dac90a4425afb67d67efbf5fb076d853
Srishti2001-eng <srishtishrivastava25@gmail.com>
2021-08-05 08:37:46 +0530

    commit

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,32 +1,42 @@
-import React from 'react'
-import Avatar from 'react-avatar';
+import React from "react";
+import Avatar from "react-avatar";
+import { Link } from "react-router-dom";
+import { deleteContact } from "../../actions/contactAction";
+import { useDispatch } from "react-redux";
 
-const Contact = ({ contact }) => {
-    const { name, phone, email } = contact;
-    return (
-        <tr>
-            <td>
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" />
-                    <label className="custom-control-label"></label>
-                </div>
-            </td>
-            <td>
-                <Avatar className="mr-5" name={name} size="45" round={true} /> {name}
-            </td>
-            <td>{phone}</td>
-            <td>{email} </td>
-            <td className="actions">
-                < a href="#" className="">
-                    <span className="material-icons mr-2 " >edit</span>
-                </a>
-                < a href="#" className="">
-                    <span className="material-icons text-danger">remove_circle</span>
-                </a>
-            </td>
-        </tr>
-
-    )
-}
+const Contact = ({ contact, selectAll }) => {
+  const dispatch = useDispatch();
+  const { name, phone, email, id } = contact;
+  return (
+    <tr>
+      <td>
+        <div className="custom-control custom-checkbox">
+          <input
+            checked={selectAll}
+            type="checkbox"
+            className="custom-control-input"
+          />
+          <label className="custom-control-label"></label>
+        </div>
+      </td>
+      <td>
+        <Avatar className="mr-2" name={name} size="45" round={true} /> {name}
+      </td>
+      <td>{phone}</td>
+      <td>{email}</td>
+      <td className="actions">
+        <Link to={`/contacts/edit/${id}`}>
+          <span className="material-icons mr-2">edit</span>
+        </Link>
+        <span
+          className="material-icons  text-danger"
+          onClick={() => dispatch(deleteContact(id))}
+        >
+          remove_circle
+        </span>
+      </td>
+    </tr>
+  );
+};
 
 export default Contact;
